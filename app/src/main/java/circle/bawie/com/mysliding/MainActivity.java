4ebce8c114c615292a4ffd8be193181e79ae0b29
fanzhangGitHub <2468242266@qq.com>
2016-11-29 13:55:39 +0800

    Initial commit

---
+++ app/src/main/java/circle/bawie/com/mysliding/MainActivity.java
@@ -0,0 +1,32 @@
+package circle.bawie.com.mysliding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final SlidingMenu menu = new SlidingMenu(this);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setBehindOffset(200);
+        menu.setFadeDegree(1);
+        TextView view = (TextView) findViewById(R.id.textView);
+        menu.attachToActivity(this,SlidingMenu.SLIDING_CONTENT);
+        View  view1 = View.inflate(this,R.layout.layout_sliding,null);
+        menu.setMenu(view1);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.showMenu(true);
+
+            }
+        });
+    }
+}
